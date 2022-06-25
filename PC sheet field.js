0f85c9e4dcbfaa91b72f6142e486c015f72dd301
Hustoroff <84928386+Hustoroff@users.noreply.github.com>
2022-06-25 23:32:06 +0300

    Movable field

--- PC sheet field.js
+++ PC sheet field.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PC sheet field
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Script for pc players
 // @author       Hustandant#8787
 // @match        *://mppclone.com/*
@@ -12,6 +12,7 @@
 // @include      *://mppfork.netlify.app/*
 // @match        *.mpp.hri7566.info/*
 // @match        *://mpp.autoplayer.space/*
+// @match        *://mpp.autoplayer.xyz/*
 // @license      MIT
 // @icon         https://github.com/Hustoroff/mpp/blob/main/icon.png?raw=true
 // @grant        none
@@ -30,7 +31,9 @@ $("#pc_btn").css({position: "absolute", left: "1020px", top: "4px"}).on("click",
         function load() {
            document.getElementById("checkSheet").checked = JSON.parse(localStorage.getItem("check"));
            document.getElementById("checkSheet").checked && sheet();
+           drag();
         }
+
         function sheet() {
            localStorage.setItem("check", JSON.parse(document.getElementById("checkSheet").checked));
            if(document.getElementById("checkSheet").checked) {
@@ -38,10 +41,40 @@ $("#pc_btn").css({position: "absolute", left: "1020px", top: "4px"}).on("click",
               resize();
            } else { document.getElementById("pcArea").value = "" }
         }
+
         function resize() {
            document.getElementById("pcArea").parentNode.parentNode.style.width = "100%";
-           localStorage.setItem("sheets", document.getElementById("pcArea").value)
+           localStorage.setItem("sheets", document.getElementById("pcArea").value);
+        }
+
+        function drag() {
+            let div = document.getElementById("Notification-area_notification").getElementsByClassName("title")[0];
+            let elem = document.getElementById("Notification-area_notification");
+            div.addEventListener("mousedown", function(evt) {
+        	evt.preventDefault();
+        	let xStart = evt.clientX;
+        	let yStart = evt.clientY;
+        	var onMouseMove = function (evtMove){
+        		evtMove.preventDefault();
+        		var xNew = xStart - evtMove.clientX;
+        		var yNew = yStart - evtMove.clientY;
+        		xStart = evtMove.clientX;
+        		yStart = evtMove.clientY;
+        		elem.style.top = (elem.offsetTop - yNew) + 'px';
+        		elem.style.left = (elem.offsetLeft - xNew) + 'px';
+        	}
+
+        	var onMouseUp = function (evtUp){
+        		document.removeEventListener('mousemove', onMouseMove);
+        		document.removeEventListener('mouseup', onMouseUp);
+        	}
+
+        	document.addEventListener('mousemove', onMouseMove)
+        	document.addEventListener('mouseup', onMouseUp)
+        })
         }
         </script>`
     });
 });
+
+
